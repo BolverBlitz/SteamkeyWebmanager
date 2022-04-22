86ac74ffc1cbee2d79239348b99e96bc901f3cc9
BolverBlitz <bolverblitz@ebg.pw>
2022-04-23 01:39:29 +0200

    Basic stuff

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const app = require('./src/app');
+const { logger } = require('./lib/logger');
+
+const port = process.env.PORT || 8000;
+//This timeout is used to delay accepting connections until the server is fully loaded. 
+//It could come to a crash if a request comes in before the settings cache was fully laoded.
+
+setTimeout(() => {
+  setTimeout(() => {
+    if (process.env.ExtraErrorWebDelay > 0) {
+      logger('info', `Webserver was delayed by ${process.env.ExtraErrorWebDelay}ms beause of a error.`);
+    }
+    app.listen(port, () => {
+      /* eslint-disable no-console */
+      logger('system', `Server started on port ${port}`);
+      /* eslint-enable no-console */
+    });
+  }, process.env.ExtraErrorWebDelay);
+}, process.env.GlobalWaitTime);
